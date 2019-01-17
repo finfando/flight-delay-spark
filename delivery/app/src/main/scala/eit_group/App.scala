73af8e8267ae02a93c3ec0a4cd3b8701cdebbc19
Filip Finfando <filip.finfando@gmail.com>
2019-01-17 18:35:19 +0100

    - code cleanup - dataframe preprocessing -- drop forbidden columns

--- delivery/app/src/main/scala/eit_group/App.scala
+++ delivery/app/src/main/scala/eit_group/App.scala
@@ -1,6 +1,4 @@
 package eit_group
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
@@ -12,55 +10,40 @@ object App {
     } else {
       val inputPath = args(0)
       val outputPath = args(1)
-      val conf = new SparkConf().setAppName("My first Spark application")
-      val sc = new SparkContext(conf)
 
-      // data is an RDD
-//      val data = sc.textFile(s"file:///${inputPath}")
-//      println(data.first())
-//      println(data.count())
-
-      // let's convert to dataframe
       val spark =
         SparkSession.builder()
           .appName("DataFrame-Basic")
-//          .master("local[4]")
+          .master("local[4]")
           .getOrCreate()
-      import spark.implicits._
-//      val flightsDF = data.toDF)
 
-      val flightsDF = spark.read.format("csv").option("header", "true").load("file:///"+inputPath)
+      // Load the data
+      val data = spark.read.format("csv")
+        .option("header", "true")
+//        .option("nullValue","null")
+//        .option("nanValue",1)
+        .load("file:///"+inputPath)
 
+      // Data preprocessing
+      val flightsDF = data.filter("ArrDelay > 0")
+        .drop("ArrTime","ActualElapsedTime","AirTime","TaxiIn","Diverted","CarrierDelay","WeatherDelay","NASDelay","SecurityDelay","LateAircraftDelay")
 
       println("*** toString() just gives you the schema")
-
       println(flightsDF.toString())
 
       println("*** It's better to use printSchema()")
-
       flightsDF.printSchema()
 
       println("*** show() gives you neatly formatted data")
-
       flightsDF.show()
 
       println("*** use select() to choose one column")
-
       flightsDF.select("ArrDelay").show()
 
-//      println("*** use select() for multiple columns")
-//
-//      flightsDF.select("sales", "state").show()
-//
-//      println("*** use filter() to choose rows")
-//
-//      flightsDF.filter($"state".equalTo("CA")).show()
-      //      val datad = data.todf
-//      val numAs = data.filter(line => line.contains("a")).count()
-//      val numBs = data.filter(line => line.contains("b")).count()
-//      println(s"Lines with a: ${numAs}, Lines with b: ${numBs}")
+      flightsDF.createOrReplaceTempView("flights")
+
+//      val sqlDF = spark.sql("SELECT * FROM people")
 //      data.repartition(1).saveAsTextFile(s"file://${outputPath}")
-      sc.stop
     }
   }
 }
