6b38430684ca05dae7d708e78bc7121f27754281
Filip Finfando <filip.finfando@gmail.com>
2019-01-20 13:21:21 +0100

    first model woooohoo

--- delivery/app/src/main/scala/eit_group/App.scala
+++ delivery/app/src/main/scala/eit_group/App.scala
@@ -2,6 +2,8 @@ package eit_group
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.feature.VectorAssembler
 
 object App {
   def main(args : Array[String]) {
@@ -32,37 +34,53 @@ object App {
 
       val hourCoder: (String => String) = (arg: String) => {if (arg.length ==2 ) "0" else if (arg.length ==3) arg.substring(0,1) else arg.substring(0,2)}
       val sqlfuncHour = udf(hourCoder)
+      // new variable: NightFlight [1,0] (from hour)
       val flightsDF = data
         .filter("Cancelled = 0")
         .filter("Diverted = 0")
         .drop("ArrTime","ActualElapsedTime","AirTime","TaxiIn","Diverted","CarrierDelay","WeatherDelay","NASDelay","SecurityDelay","LateAircraftDelay")
         .drop("Cancelled","Diverted","CancellationCode","TailNum","FlightNum","Year","DayOfMonth")
         .withColumn("Hour", sqlfuncHour(col("CRSDepTime")))
+        .withColumn("DepDelay", col("DepDelay").cast("Double"))
+        .withColumn("ArrDelay", col("ArrDelay").cast("Double"))
+
       println("Count after preprocessing")
       println(flightsDF.count())
-      // new variable: NightFlight [1,0] (from hour)
 
-      //Transformations
+      println("*** toString() just gives you the schema")
+      println(flightsDF.toString())
+      println("*** It's better to use printSchema()")
+      flightsDF.printSchema()
+      println("*** show() gives you neatly formatted data")
+      flightsDF.show()
+      println("*** use select() to choose one column")
+      flightsDF.select("ArrDelay").show()
 
-      //Models - MLlib
+      val assembler = new VectorAssembler()
+        .setInputCols(Array("DepDelay"))
+        .setOutputCol("features")
+      val output = assembler.transform(flightsDF.select("DepDelay","ArrDelay"))
+      output.show(truncate=false)
+
+      val lr = new LinearRegression()
+        .setFeaturesCol("features")
+        .setLabelCol("ArrDelay")
+        .setMaxIter(10)
+        .setElasticNetParam(0.8)
+      val lrModel = lr.fit(output)
+      println(s"Coefficients: ${lrModel.coefficients}")
+      println(s"Intercept: ${lrModel.intercept}")
+      val trainingSummary = lrModel.summary
+      println(s"numIterations: ${trainingSummary.totalIterations}")
+      println(s"objectiveHistory: ${trainingSummary.objectiveHistory.toList}")
+      trainingSummary.residuals.show()
+      println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+      println(s"r2: ${trainingSummary.r2}")
 
 
       //Models validation
 
-      println("*** toString() just gives you the schema")
-      println(flightsDF.toString())
-
-      println("*** It's better to use printSchema()")
-      flightsDF.printSchema()
-
-      println("*** show() gives you neatly formatted data")
-      flightsDF.show()
-
-      println("*** use select() to choose one column")
-      flightsDF.select("ArrDelay").show()
-
-      flightsDF.createOrReplaceTempView("flights")
-
+//      flightsDF.createOrReplaceTempView("flights")
 //      val sqlDF = spark.sql("SELECT * FROM people")
 //      flightsDF.repartition(1).saveAsTextFile(s"file://${outputPath}")
     }
